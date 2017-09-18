b39871cd004d5fe3d72b133909a18e7f4df23eb3
Siarhei Bialevich <acharne1985@gmail.com>
2017-09-18 06:13:34 +0300

    Lection 2 task 2

---
+++ Lection2/task2/Clinic.java
@@ -0,0 +1,8 @@
+public class Clinic {
+    public static void main(String[] args) {
+        Patient patient = new Patient();
+        Address address = new Address();
+        Employment employment = new Employment();
+        MedicalHistory medicalHistory = new MedicalHistory();
+    }
+}
