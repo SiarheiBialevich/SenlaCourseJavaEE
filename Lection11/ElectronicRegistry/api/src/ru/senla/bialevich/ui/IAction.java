078f746a485049b55732b4b39c4da9b16be7bc81
Siarhei Bialevich <acharne1985@gmail.com>
2017-12-11 02:50:56 +0300

    l9

--- Lection11/ElectronicRegistry/api/src/ru/senla/bialevich/ui/IAction.java
+++
@@ -1,7 +0,0 @@
-package ru.senla.bialevich.ui;
-
-import ru.senla.bialevich.request.src.ru.senla.bialevich.IRequestHandler;
-
-public interface IAction {
-    void execute(IRequestHandler requestHandler);
-}
